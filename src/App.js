a38a203bcb9e0888661a1839ea54ceb423f8f0ef
Taylor <hinestaylor29@gmail.com>
2020-11-15 21:27:00 -0800

    added code attribution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Weather from "./Weather";
+import Attribution from "./Attribution";
 
 export default function App() {
   return (
     <div className="App">
       <Weather />
+      < Attribution />
     </div>
   );
 }
---
+++ src/Attribution.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Attribution() {
+    return(
+        <p>
+            <a href="https://github.com/hinestaylor29/weather-app-react"> Open-source code </a> by Taylor Hines 🤠
+        </p>
+    );
+}
