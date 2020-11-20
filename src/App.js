08c593b5aecc065773459cb04a3bf69ff8f463fb
Taylor <hinestaylor29@gmail.com>
2020-11-20 12:55:29 -0800

    Implemented axios and integrated api

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
+      <Weather defaultCity= "New York"/>
       </div>
      <footer className="footer">
             <a href="https://github.com/hinestaylor29/weather-app-react"> Open-source code </a> by Taylor Hines 🤠
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,27 @@
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from 'axios';
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    date: "October 12, 2020",
-    description: "Sunny",
-    humidity: "20%",
-    wind: "10 km/h",
-    imgUrl: "https://www.flaticon.com/free-icon/sun_169367"
-  };
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind:response.data.wind.speed,
+      city: response.data.name,
+      humitidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: response.data.weather[0].icon,
+      date: "Wednesday 10:00",
+    }); 
+  }
 
-  return (
-    <div>
-      <div className="row">
+  if (weatherData.ready) {
+    return (
+  <div>
+    <div className="row">
         <form className="location-search col-12 form-group">
           <input
             className="search-bar col-6"
@@ -33,15 +42,15 @@ export default function Weather() {
       <div className="row">
         <div className="col-4">
           <div className="float-right">
-            <img src={weatherData.imgUrl} alt="" />
+          <img src={weatherData.iconUrl} id="icon" />
           </div>
         </div>
 
         <div className="col-6">
-          <h1> 19 </h1>
+          <h1> {Math.round(temperature)} C </h1>
           <h1> {weatherData.city} </h1>
 
-          <h4>{weatherData.city}</h4>
+          <h4 className="text-capitalize">{weatherData.description}</h4>
 
           <h4> {weatherData.city} </h4>
 
@@ -51,10 +60,19 @@ export default function Weather() {
             </a>{" "}
             |<a href="/">°F</a>
           </span>
-          <p className="metadata"> {weatherData.humidity}</p>
-          <p className="metadata"> {weatherData.wind}</p>
+          <p className="metadata"> {weatherData.humidity}%</p>
+          <p className="metadata"> {weatherData.wind}km/h</p>
         </div>
       </div>
     </div>
-  );
+      );
+  } else {
+  let apiKey = "85ad33b3f2d949047f19cf1a73113630";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading...";
+}
+
 }
