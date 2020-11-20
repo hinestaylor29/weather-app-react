9e00dadb9deb2934fd67bb42d5f04ac7a2229402
Taylor <hinestaylor29@gmail.com>
2020-11-19 23:32:20 -0800

    removed target because it wouldn't deploy

--- src/Attribution.js
+++ src/Attribution.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Attribution() {
     return(
         <p className="open-source">
-            <a href="https://github.com/hinestaylor29/weather-app-react" target="_blank"> Open-source code </a> by Taylor Hines 🤠
+            <a href="https://github.com/hinestaylor29/weather-app-react"> Open-source code </a> by Taylor Hines 🤠
         </p>
     );
 }
